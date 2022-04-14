0d71c4a00339e0f42c649e7300caed6832939c26
seathree <park.yang.ho@gmail.com>
2022-04-14 10:57:24 +0900

    final

---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
+import Link from "next/link";
+import { useRouter } from "next/router";
+const API_KEY = "5234fe25f1dc6a4fa214bb332ee8240c";
+
+export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id) => {
+    router.push(`/movies/${id}`);
+  };
+  return (
+    <div>
+      <Seo title="Home"></Seo>
+      {results?.map((movie) => (
+        <div onClick={() => onClick(movie.id)} className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>
+            <Link href={`/movies/${movie.id}`}>
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
+        </div>
+      ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
+}
